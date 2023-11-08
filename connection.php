f0eee5f15434e141be1db46f78f4cc2c7d9d3972
guizzoS2 <guilvieira409@gmail.com>
2023-11-08 13:41:24 -0300

    feito: banco, conexao, login salvo e cont visits

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+    $usuario = 'user de cadasto';
+    $senha = 'senha utilizada na conexao';
+    $database = 'banco de dados a ser utilizado';
+    $host ='ip + :porta';
+
+    $mysqli = new mysqli($host, $usuario, $senha, $database);
+
+    if ($mysqli->connect_error) {
+        die('Falha ao conectar ao banco de dados:'. $mysqli->connect_error);
+    }
+?>
